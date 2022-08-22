c9cb514a2142f2dc648de3385e5fdddb96e5b4dd
LAPTOP-0PJE2SB0\marko <marko.vresk4520@gmail.com>
2022-08-22 17:46:04 +0200

    Izrada dizajna forme za prijavu.

---
+++ Software/Registracija i Prijava/Registracija i Prijava/Prijava.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Registracija_i_Prijava
+{
+    partial class Prijava
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonPrijavise = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textBoxKorisničkoIme = new System.Windows.Forms.TextBox();
+            this.textBoxLozinka = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 40.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 76);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Prijava";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(113, 317);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 4);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 4);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(113, 411);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(300, 4);
+            this.panel3.TabIndex = 8;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // buttonPrijavise
+            // 
+            this.buttonPrijavise.BackColor = System.Drawing.Color.DarkGray;
+            this.buttonPrijavise.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonPrijavise.Location = new System.Drawing.Point(110, 474);
+            this.buttonPrijavise.Name = "buttonPrijavise";
+            this.buttonPrijavise.Size = new System.Drawing.Size(300, 64);
+            this.buttonPrijavise.TabIndex = 9;
+            this.buttonPrijavise.Text = " Prijavi se";
+            this.buttonPrijavise.UseVisualStyleBackColor = false;
+            this.buttonPrijavise.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(173, 556);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 28);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Registrirajte se";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Book Antiqua", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(221, 619);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 22);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Izlaz";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Registracija_i_Prijava.Properties.Resources.lozinka;
+            this.pictureBox6.Location = new System.Drawing.Point(46, 372);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(50, 43);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Registracija_i_Prijava.Properties.Resources.ikona3;
+            this.pictureBox5.Location = new System.Drawing.Point(46, 278);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 43);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 1;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Registracija_i_Prijava.Properties.Resources.vik;
+            this.pictureBox4.Location = new System.Drawing.Point(159, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(179, 165);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // textBoxKorisničkoIme
+            // 
+            this.textBoxKorisničkoIme.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxKorisničkoIme.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxKorisničkoIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxKorisničkoIme.Location = new System.Drawing.Point(113, 278);
+            this.textBoxKorisničkoIme.Multiline = true;
+            this.textBoxKorisničkoIme.Name = "textBoxKorisničkoIme";
+            this.textBoxKorisničkoIme.Size = new System.Drawing.Size(300, 40);
+            this.textBoxKorisničkoIme.TabIndex = 12;
+            this.textBoxKorisničkoIme.Text = "Korisničko ime";
+            this.textBoxKorisničkoIme.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxKorisničkoIme.TextChanged += new System.EventHandler(this.textBoxKorisničkoIme_TextChanged);
+            // 
+            // textBoxLozinka
+            // 
+            this.textBoxLozinka.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxLozinka.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxLozinka.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxLozinka.Location = new System.Drawing.Point(110, 372);
+            this.textBoxLozinka.Multiline = true;
+            this.textBoxLozinka.Name = "textBoxLozinka";
+            this.textBoxLozinka.Size = new System.Drawing.Size(300, 40);
+            this.textBoxLozinka.TabIndex = 13;
+            this.textBoxLozinka.Text = "Lozinka";
+            this.textBoxLozinka.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxLozinka.TextChanged += new System.EventHandler(this.textBoxLozinka_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Book Antiqua", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(360, 435);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 22);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Prikaži lozinku";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Prijava
+            // 
+            this.ClientSize = new System.Drawing.Size(519, 705);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxLozinka);
+            this.Controls.Add(this.textBoxKorisničkoIme);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonPrijavise);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Prijava";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonPrijavise;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxKorisničkoIme;
+        private System.Windows.Forms.TextBox textBoxLozinka;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Software/Registracija i Prijava/Registracija i Prijava/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registracija_i_Prijava
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Prijava());
+        }
+    }
+}
